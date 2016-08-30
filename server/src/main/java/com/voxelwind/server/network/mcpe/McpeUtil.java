8cf037ff00d26e77eea0d336a53b4c955383cb98
Tux <write@imaginarycode.com>
2016-08-30 16:32:26 -0400

    Added functions to read varints

--- server/src/main/java/com/voxelwind/server/network/mcpe/McpeUtil.java
+++ server/src/main/java/com/voxelwind/server/network/mcpe/McpeUtil.java
@@ -241,4 +241,28 @@ public class McpeUtil {
         buf.writeLong(uuid.getMostSignificantBits());
         buf.writeLong(uuid.getLeastSignificantBits());
     }
+
+    public static int readVarInt(ByteBuf in) throws IOException {
+        int i = 0;
+        int j = 0;
+        while (true) {
+            int k = in.readByte();
+            i |= (k & 0x7F) << j++ * 7;
+            if (j > 5) throw new RuntimeException("VarInt too big");
+            if ((k & 0x80) != 128) break;
+        }
+        return i;
+    }
+
+    public static void writeVarInt(ByteBuf out, int paramInt) throws IOException {
+        while (true) {
+            if ((paramInt & 0xFFFFFF80) == 0) {
+                out.writeByte(paramInt);
+                return;
+            }
+
+            out.writeByte(paramInt & 0x7F | 0x80);
+            paramInt >>>= 7;
+        }
+    }
 }
