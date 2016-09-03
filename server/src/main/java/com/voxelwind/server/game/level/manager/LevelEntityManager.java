ac0d5522d79f6617dd32b0b76806c18a7df6a5c7
Tux <write@imaginarycode.com>
2016-09-03 14:30:49 -0400

    This packet isn't a list
    Besides, we only ever send one item.

--- server/src/main/java/com/voxelwind/server/game/level/manager/LevelEntityManager.java
+++ server/src/main/java/com/voxelwind/server/game/level/manager/LevelEntityManager.java
@@ -63,7 +63,8 @@ public class LevelEntityManager {
                     level.getPacketManager().queuePacketForViewers(entity, moveEntityPacket);
 
                     McpeSetEntityMotion motionPacket = new McpeSetEntityMotion();
-                    motionPacket.getMotionList().add(new McpeSetEntityMotion.EntityMotion(entity.getEntityId(), entity.getMotion()));
+                    motionPacket.setEntityId(entity.getEntityId());
+                    motionPacket.setMotion(entity.getMotion());
                     level.getPacketManager().queuePacketForViewers(entity, motionPacket);
 
                     entity.resetStale();
--- server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeSetEntityMotion.java
+++ server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeSetEntityMotion.java
@@ -11,40 +11,18 @@ import java.util.List;
 
 @Data
 public class McpeSetEntityMotion implements NetworkPackage {
-    private final List<EntityMotion> motionList = new ArrayList<>();
+    private long entityId;
+    private Vector3f motion;
 
     @Override
     public void decode(ByteBuf buffer) {
-        while (buffer.isReadable()) {
-            long entityId = McpeUtil.readUnsignedVarInt(buffer);
-            Vector3f motion = McpeUtil.readVector3f(buffer);
-            motionList.add(new EntityMotion(entityId, motion));
-        }
+        entityId = McpeUtil.readUnsignedVarInt(buffer);
+        motion = McpeUtil.readVector3f(buffer);
     }
 
     @Override
     public void encode(ByteBuf buffer) {
-        for (EntityMotion motion : motionList) {
-            McpeUtil.writeUnsignedVarInt(buffer, (int) motion.entityId);
-            McpeUtil.writeVector3f(buffer, motion.motion);
-        }
-    }
-
-    public static class EntityMotion {
-        private final long entityId;
-        private final Vector3f motion;
-
-        public EntityMotion(long entityId, Vector3f motion) {
-            this.entityId = entityId;
-            this.motion = motion;
-        }
-
-        public long getEntityId() {
-            return entityId;
-        }
-
-        public Vector3f getMotion() {
-            return motion;
-        }
+        McpeUtil.writeUnsignedVarInt(buffer, (int) entityId);
+        McpeUtil.writeVector3f(buffer, motion);
     }
 }
